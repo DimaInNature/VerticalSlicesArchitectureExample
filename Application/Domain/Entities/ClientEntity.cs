721fdac44c6ba89b63b1679cc3b1e66e9e12ba58
Dmitry Nikulin <Nikulin.d@dobrozaim.ru>
2022-11-05 15:13:38 +0300

    Добавьте файлы проекта.

---
+++ Application/Domain/Entities/ClientEntity.cs
@@ -0,0 +1,12 @@
+﻿namespace Application.Domain.Entities;
+
+public class ClientEntity
+{
+    public Guid Id { get; set; }
+
+    public string? FirstName { get; set; }
+
+    public string? LastName { get; set; }
+
+    public IEnumerable<SaleContractEntity>? SellingHistory { get; set; }
+}
---
+++ Application/Domain/Entities/SaleContractEntity.cs
@@ -0,0 +1,16 @@
+﻿namespace Application.Domain.Entities;
+
+public class SaleContractEntity
+{
+    public Guid Id { get; set; }
+
+    public Guid? ClientId { get; set; }
+
+    public ClientEntity? Client { get; set; }
+
+    public Guid? ProductId { get; set; }
+
+    public ProductEntity? Product { get; set; }
+
+    public decimal Cost { get; set; }
+}
