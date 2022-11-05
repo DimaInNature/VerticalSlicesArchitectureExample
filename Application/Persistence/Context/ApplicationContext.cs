01e2fec05c232aa3f3864e03915a8236199af5ff
Dmitry Nikulin <Nikulin.d@dobrozaim.ru>
2022-11-05 15:22:42 +0300

    feat: Create DbContext.

---
+++ Application/Persistence/Context/ApplicationContext.cs
@@ -0,0 +1,20 @@
+﻿namespace Application.Persistence.Context;
+
+public class ApplicationContext : DbContext
+{
+    public DbSet<ClientEntity> Clients => Set<ClientEntity>();
+
+    public DbSet<ProductEntity> Products => Set<ProductEntity>();
+
+    public DbSet<SaleContractEntity> SaleContract => Set<SaleContractEntity>();
+
+    public ApplicationContext(DbContextOptions options) : base(options) 
+    {
+        Database.EnsureCreated();
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+}
